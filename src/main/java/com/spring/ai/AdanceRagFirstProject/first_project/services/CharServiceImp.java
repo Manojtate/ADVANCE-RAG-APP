fd4ed5911098f01261e5c21bf98386aaa35f1e73
Manoj Tate <manojtate@gmail.com>
2026-06-04 22:48:13 +0530

    how it pre retrieval , retrieval and post retrieval work using  RetrievalAugmentationAdvisor

--- src/main/java/com/spring/ai/AdanceRagFirstProject/first_project/services/CharServiceImp.java
+++ src/main/java/com/spring/ai/AdanceRagFirstProject/first_project/services/CharServiceImp.java
@@ -6,7 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.generation.augmentation.ContextualQueryAugmenter;
+import org.springframework.ai.rag.preretrieval.query.expansion.MultiQueryExpander;
 import org.springframework.ai.rag.preretrieval.query.transformation.RewriteQueryTransformer;
+import org.springframework.ai.rag.preretrieval.query.transformation.TranslationQueryTransformer;
+import org.springframework.ai.rag.retrieval.join.ConcatenationDocumentJoiner;
 import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,12 +55,15 @@ public class CharServiceImp implements  ChatService {
     @Override
     public String getResponse(String userQuery) {
 
-         RetrievalAugmentationAdvisor.builder()
+      var advisor =   RetrievalAugmentationAdvisor.builder()
                  .queryTransformers(
                          RewriteQueryTransformer.builder()
                                  .chatClientBuilder(chatClient.mutate().clone())
-                                 .build()
+                                 .build(),
+                         TranslationQueryTransformer.builder().chatClientBuilder(chatClient.mutate().clone()).targetLanguage("english").build()
+
                  )
+                 .queryExpander(MultiQueryExpander.builder().chatClientBuilder(chatClient.mutate().clone()).numberOfQueries(3).build())
                  .documentRetriever(
                          VectorStoreDocumentRetriever.builder()
                                  .vectorStore(vectorStore)
@@ -64,12 +71,15 @@ public class CharServiceImp implements  ChatService {
                                  .similarityThreshold(0.3)
                                  .build()
                  )
+                 .documentJoiner(new ConcatenationDocumentJoiner())
+                 .queryAugmenter(ContextualQueryAugmenter.builder().build())
                  .build();
 
 
 
         return   chatClient
                 .prompt()
+                .advisors(advisor)
                 .user(userQuery)
                 .call()
                 .content();
